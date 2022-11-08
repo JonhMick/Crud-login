06cbdf0c37dbbfcd9eb7b6c0b0ade66d22adbc10
Jonatas Barbosa Carneiro <108531414+JonhMick@users.noreply.github.com>
2022-11-08 13:50:28 -0300

    Add files via upload

---
+++ config.php
@@ -0,0 +1,19 @@
+<?php
+
+    $dbHost = 'localhost';
+    $dbUsername = 'id19411713_jonatas';
+    $dbPassword = ']*[-P(4aP26euR>(';
+    $dbName = 'id19411713_crud';
+    
+    $conexao = new mysqli($dbHost,$dbUsername,$dbPassword,$dbName);
+
+    // if($conexao->connect_errno)
+    // {
+    //     echo "Erro";
+    // }
+    // else
+  
+    //     echo "Conexão efetuada com sucesso";
+
+
+?>
